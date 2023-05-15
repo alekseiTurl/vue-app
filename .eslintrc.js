1d7202f5d95af09ee4c5492c3ed8ec8862709138
Alex Turlaykov <124469993+alekseiTurl@users.noreply.github.com>
2023-05-15 22:59:15 +0300

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'no-promise-executor-return': 'off',
+    'no-shadow': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': [
+      'error',
+      {
+        code: 200,
+        tabWidth: 2,
+        ignoreComments: true, // "comments": 80
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+  },
+
+};
